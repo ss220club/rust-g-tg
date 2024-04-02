1b7992e10cade07fffcff57c6f5d5ae7e218a6c3
Dimach <dimach2142@gmail.com>
2024-04-02 04:43:51 +0300

    Not cache git repository when not needed

--- src/git.rs
+++ src/git.rs
@@ -1,26 +1,19 @@
 use chrono::{TimeZone, Utc};
-use gix::{open::Error as OpenError, Repository};
-
-thread_local! {
-    static REPOSITORY: Result<Repository, OpenError> = gix::open(".");
-}
 
 byond_fn!(fn rg_git_revparse(rev) {
-    REPOSITORY.with(|repo| -> Option<String> {
-        let repo = repo.as_ref().ok()?;
-        let object = repo.rev_parse_single(rev).ok()?;
-        Some(object.to_string())
-    })
+    let repo = gix::open(".");
+    let repo = repo.as_ref().ok()?;
+    let object = repo.rev_parse_single(rev).ok()?;
+    Some(object.to_string())
 });
 
 byond_fn!(fn rg_git_commit_date(rev) {
-    REPOSITORY.with(|repo| -> Option<String> {
-        let repo = repo.as_ref().ok()?;
-        let rev = repo.rev_parse_single(rev).ok()?;
-        let object = rev.object().ok()?;
-        let commit = object.try_into_commit().ok()?;
-        let commit_time = commit.committer().ok()?.time;
-        let datetime = Utc.timestamp_opt(commit_time.seconds, 0).latest()?;
-        Some(datetime.format("%F").to_string())
-    })
+    let repo = gix::open(".");
+    let repo = repo.as_ref().ok()?;
+    let rev = repo.rev_parse_single(rev).ok()?;
+    let object = rev.object().ok()?;
+    let commit = object.try_into_commit().ok()?;
+    let commit_time = commit.committer().ok()?.time;
+    let datetime = Utc.timestamp_opt(commit_time.seconds, 0).latest()?;
+    Some(datetime.format("%F").to_string())
 });
